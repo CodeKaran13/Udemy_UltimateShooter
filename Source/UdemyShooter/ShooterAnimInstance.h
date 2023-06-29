45f167546fe274d870756d2b802a5b446d094682
Karan Nandkar <knandkar007@gmail.com>
2023-06-29 13:45:21 +0530

    Turn in place animations

--- Source/UdemyShooter/ShooterAnimInstance.h
+++ Source/UdemyShooter/ShooterAnimInstance.h
@@ -16,10 +16,17 @@ class UDEMYSHOOTER_API UShooterAnimInstance : public UAnimInstance
 
 public:
 
+	UShooterAnimInstance();
+
 	UFUNCTION(BlueprintCallable)
 		void UpdateAnimationProperties(float DeltaTime);
 
 	virtual void NativeInitializeAnimation() override;
+
+protected:
+
+	// Handle turning in place variables
+	void TurnInPlace();
 
 private:
 
@@ -49,4 +56,13 @@ private:
 	/* */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 		bool bAiming;
+
+	// Yaw of the Character this frame
+	float CharacterYaw;
+
+	// Yaw of the Character the previous frame
+	float CharacterYawLastFrame;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn In Place", meta = (AllowPrivateAccess = "true"))
+		float RootYawOffset;
 };
