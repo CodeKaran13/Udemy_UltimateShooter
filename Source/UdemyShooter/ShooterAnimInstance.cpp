65db67e4346fd5144a29ee5723598a3062fc0107
Karan Nandkar <knandkar007@gmail.com>
2023-07-02 12:30:29 +0530

    recoil weight fixed when turning in place

--- Source/UdemyShooter/ShooterAnimInstance.cpp
+++ Source/UdemyShooter/ShooterAnimInstance.cpp
@@ -21,7 +21,9 @@ UShooterAnimInstance::UShooterAnimInstance() :
 	OffsetState(EOffsetState::EOS_Hip),
 	CharacterRotation(FRotator(0)),
 	CharacterRotationLastFrame(FRotator(0)),
-	YawDelta(0.f)
+	YawDelta(0.f),
+	RecoilWeight(1.f),
+	bTurningInPlace(false)
 {
 
 }
@@ -132,6 +134,7 @@ void UShooterAnimInstance::TurnInPlace()
 		const float Turning = GetCurveValue(TEXT("Turning"));
 		if (Turning > 0)
 		{
+			bTurningInPlace = true;
 			RotationCurveLastFrame = RotationCurve;
 			RotationCurve = GetCurveValue(TEXT("Rotation"));
 			const float DeltaRotation = RotationCurve - RotationCurveLastFrame;
@@ -144,6 +147,47 @@ void UShooterAnimInstance::TurnInPlace()
 			{
 				const float YawExcess = AbsRootYawOffset - 90.f;
 				RootYawOffset > 0 ? RootYawOffset -= YawExcess : RootYawOffset += YawExcess;
+			}
+		}
+		else
+		{
+			bTurningInPlace = false;
+		}
+
+		if (bTurningInPlace)
+		{
+			if (bReloading)
+			{
+				RecoilWeight = 1.f;
+			}
+			else
+			{
+				RecoilWeight = 0.f;
+			}
+		}
+		else
+		{
+			if (bCrouching)
+			{
+				if (bReloading)
+				{
+					RecoilWeight = 1.f;
+				}
+				else
+				{
+					RecoilWeight = 0.1f;
+				}
+			}
+			else
+			{
+				if (bAiming || bReloading)
+				{
+					RecoilWeight = 1.f;
+				}
+				else
+				{
+					RecoilWeight = 0.5f;
+				}
 			}
 		}
 	}
