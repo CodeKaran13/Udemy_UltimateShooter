2ccda98ba83f19e02329f5ea94a3eb9cc4a3bdd3
Karan Nandkar <knandkar007@gmail.com>
2023-07-03 17:06:37 +0530

    Overriding SetItemProperties in Item

---
+++ Source/UdemyShooter/Ammo.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Ammo.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class UDEMYSHOOTER_API AAmmo : public AItem
+{
+	GENERATED_BODY()
+
+public:
+
+	AAmmo();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	// Override of Set properties so we can set AmmoMesh properties
+	virtual void SetItemProperties(EItemState State) override;
+
+private:
+
+	// Mesh for the ammo pickup
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Ammo, meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* AmmoMesh;
+
+public:
+
+	FORCEINLINE UStaticMeshComponent* GetAmmoMesh() const { return AmmoMesh; }
+};
